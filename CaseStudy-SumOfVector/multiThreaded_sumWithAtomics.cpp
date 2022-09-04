4fee40d0639f4191d9b3c54ed4c2f7582b7c9aae
mrigank-iitkgp <mrigank432@gmail.com>
2022-09-04 19:07:46 +0530

    Multi threaded summation using atomics

---
+++ CaseStudy-SumOfVector/multiThreaded_sumWithAtomics.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <random>
+#include <mutex>
+#include <thread>
+
+using namespace std;
+typedef long long int ll;
+
+atomic < ll > sum = {};
+
+constexpr ll sz = 100000000;
+constexpr ll fir =  25000000;
+constexpr ll sec =  50000000;
+constexpr ll thi =  75000000;
+constexpr ll fou = 100000000;
+
+void summation(vector < int >& arr , atomic < ll > & sum , ll start , ll end) {
+    for(ll i = start ; i < end ; i++) {
+        sum += arr[i];
+    }
+}
+
+int main() {
+    cout << "Calculating time for Sum of Vectors using multiple threads with atomics!!" << endl;
+
+    // Calculating the sum of random values (uniform distribution(1 , 10)) of vector using Loops
+
+    vector < int > randomValues;
+    randomValues.reserve(sz);
+    random_device seed;
+    mt19937 engine(seed());
+    uniform_int_distribution < int > uniformDist(1 , 10);
+    for(ll i = 0 ; i < sz ; i++) {
+        randomValues.push_back(uniformDist(engine));
+    }
+    const auto start = chrono::steady_clock::now();
+    thread t1(summation , ref(randomValues) , ref(sum) , 0ll , fir);
+    thread t2(summation , ref(randomValues) , ref(sum) , fir , sec);
+    thread t3(summation , ref(randomValues) , ref(sum) , sec , thi);
+    thread t4(summation , ref(randomValues) , ref(sum) , thi , fou);
+
+    t1.join();
+    t2.join();
+    t3.join();
+    t4.join();
+
+    chrono::duration < double > dur = chrono::steady_clock::now() - start;
+    cout << "Total duration : " << dur.count() << " seconds" << endl;
+    cout << "Sum : " << sum << endl;
+}
